374f5f37ff8806d3d928f5ede63b765620dd6d8b
cooksoundscapes <paulobmedina@gmail.com>
2021-12-23 16:53:36 -0300

    added error tracking to object state

--- src/App.js
+++ src/App.js
@@ -3,27 +3,42 @@ import AreaForm from "./AreaForm";
 
 const parede = {
     largura: 3,
-    altura: 2.5,
+    altura: 2.5,    
     portas: 0,
     janelas: 0,
     rules: {
         porta: [0.8, 1.9],
         janela: [2.0, 1.2],
-        alturaMinima: (parede) => {
-            let min = 1, max = 15;
+        metragemMinima: (parede) => {
+            let min = 1;
+            let maxAlt = 15/parede.largura;
+            let maxLarg = 15/parede.altura;
             if (parede.portas > 0) {
-                min = porta[1] + 0.3;
+                min = parede.rules.porta[1] + 0.3;
             }
-            return [min, max];
+            parede.altura = Math.max(min, parede.altura);
+            parede.largura = Math.max(min, parede.largura);
+            if (parede.altura > maxAlt || parede.largura > maxLarg) {
+                return [["largura", "altura"], "Área máxima de 15m² excedida"];
+            }
+            return null;
+        },
+        maximaAreaVazada: (parede) => {
+            const {porta, janela} = parede.rules;
+            const areaVazada = 
+                porta[0] * porta[1] * parede.portas +
+                janela[0] * janela[1] * parede.janelas;
+            const areaSolida = parede.altura * parede.largura;
+            if (areaSolida/2 < areaVazada) {
+                return [["portas", "janelas"], "Atenção: área vazada máxima excedida."]; 
+            }
+            return null;
         }
     }
 }
 
 const App = () => (
-    <div>
-        <AreaForm initAreas={4} areaConfig={parede} />
-        
-    </div>
+    <AreaForm initAreas={4} areaConfig={parede} />
 )
 
 export default App;
--- src/AreaForm.js
+++ src/AreaForm.js
@@ -4,26 +4,48 @@ import immer from 'immer';
 
 const reducerActions = {
     add: (state, action) => {
-        state.areas.push(action.wallModel);
+        state.areas.push(action.areaModel);
     },
     delete: (state, action) => {
         state.areas.splice(action.index, 1);
     },
     change: (state, action) => {
-        state.areas[action.row][action.param] = parseFloat(action.value);
-    }
-}
+        state.areas[action.row][action.param] = Math.max(0, parseFloat(action.value));
 
-const initState = {
-    areas: []
+        const errState = state.error[action.row];
+        const tests = Object.entries(action.rules).filter(r => typeof(r[1]) == 'function');
+        //test area against all conditions on descriptor.rules;
+        tests.forEach(([fname, func]) => {
+            let err, errIndex;
+            if (err = func(state.areas[action.row])) {
+                //if error isn't in state;
+                if ((errIndex = errState.findIndex(e => e.name === fname)) < 0) {
+                    errState.push({name: fname, error: err})
+                }
+            }  else {
+                //if error exists, remove it!
+                if ((errIndex = errState.findIndex(e => e.name === fname)) >= 0) {
+                    errState.splice(errIndex, 1);
+                } 
+            }
+        })
+        state.error[action.row] = errState;
+    }
 }
 
 function init({initAreas, areaConfig}) {
+    const initState = {
+        areas: [],
+        error: []
+    }
     if (initAreas) {
         for (let i = 0; i < initAreas; i++) {
+            console.log("adding", i)
             initState.areas.push(areaConfig);
+            initState.error.push([])
         }
     }
+    return initState;
 }
 
 function reducer(state, action) {
@@ -33,7 +55,7 @@ function reducer(state, action) {
 }
 
 const AreaForm = props => {
-    const [state, dispatch] = useReducer(reducer, initState, init(props));
+    const [state, dispatch] = useReducer(reducer, props, init);
 
     const addRow = () => {
         dispatch({
@@ -49,18 +71,21 @@ const AreaForm = props => {
         })
     }
 
-    const changeValue = (value, row, param) => {
+    const changeValue = (value, row, param, rules) => {
+        console.log(state.error[row])
+        if (!value) return;
         dispatch({
             type: "change",
             row: row,
             param: param,
-            value: value
+            value: value,
+            rules: rules
         })
     }
 
-    return state.areas.map( (wall, i) => {
+    return state.areas.map( (area, i) => {
         //only accepts primitive values from keys;
-        const params = Object.entries(wall).filter( p => typeof(p[1]) != "object");
+        const params = Object.entries(area).filter( p => typeof(p[1]) != "object");
         return (
             <div key={i}>
                 <button onClick={() => deleteRow(i)}
@@ -71,7 +96,7 @@ const AreaForm = props => {
                         <React.Fragment key={ind}>
                             <label>{p[0]+': '}</label>
                             <input type="number"
-                                onChange={e => changeValue(e.target.value, i, p[0])} 
+                                onChange={e => changeValue(e.target.value, i, p[0], area.rules)} 
                                 value={p[1]}/>
                         </React.Fragment>
                         )
